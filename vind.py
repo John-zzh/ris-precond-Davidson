8300a11a6b6b20a030e7cb4bb53c2cf922727dfe
John Zhou <zzz6@case.edu>
2020-03-31 16:06:19 -0400

    new frame

--- vind.py
+++ vind.py
@@ -3,7 +3,7 @@ import numpy as np
 from pyscf import gto, scf, dft, tddft
 import davidson4
 mol = gto.Mole()
-mol.build(atom = 'H 0 0 0; O 0 0 1.2;H 0.8 0 1.2', basis = '631g', symmetry = True)
+mol.build(atom = 'H 0 0 0; F 0 0 1.2', basis = '631g', symmetry = True)
 mf = dft.RKS(mol) #RKS, restrict the geometry, no optimization #mf is ground density?
 mf.xc = 'b3lyp'
 mf.kernel()  #single point energy
@@ -16,72 +16,70 @@ print ('Pyscf time =', round(end-start,4))
 #mytd.kernel()
 
 vind, hdiag = td.gen_vind(mf)
-n = len(hdiag) #n is size of Hamiltonian
-
+n = len(hdiag)
 print ('size of H is', n)
 
-#Gram-Schimidt block,
-def orthonormal (v1, v2):
-    for i in range (0,n):
-        v2 = v2 - (np.dot(v1, v2) / np.linalg.norm(v1)) * v1
-        norm = np.linalg.norm(v2)
-        av2 = np.maximum (v2, -v2)
-        asort = av2.argsort()
-        if norm < 1e-8:
-            v2 [int(np.argwhere(asort == i))] = 1e-8/(np.sqrt(n))
-        else:
-            break
-    v2 = v2/norm
-    return v2
 #davidson block
-def davidson(vind, eig): # matrix A and how many eignvalues to solve
+def davidson(vind, k): # fucntion vind and how many eignvalues to solve
     start = time.time()
     tol = 1e-8      # Convergence tolerance
-    k = eig         # number of expension for each iteration
-    mmax = 90      # Maximum number of iterations
-    #print ('Amount of Eigenvalues we want:', eig)
-    V = np.zeros((n,30*eig)) #array of zeros. a container to hold guess vectors
-    W = np.zeros((n,30*eig)) #array of zeros. a container to hold transformed guess vectors
+    max = 90      # Maximum number of iterations
+    V = np.zeros((n,30*k)) #array of zeros. a container to hold guess vectors
+    W = np.zeros((n,30*k)) #array of zeros. a container to hold transformed guess vectors
+  
     # Begin iterations
-    Iteration = 0
-    for m in range(k,mmax,k):
+    Iteration   = 0
+    
+    m = 2*k  # m is size of initial subspace Hamiltonian
+    lasit_newvec = 0  #amount of new vector added in last iteration
+    for i in range(0, max):
         Iteration = Iteration + 1
-        #print ('Iteration =', Iteration)
-        if m == k:
-            ahdiag = np.maximum (hdiag, - hdiag) #ahdiag is a all-values-positive version of hdiag
-            sort = ahdiag.argsort()
-            for i in range(0,k):
-                V[int(np.argwhere(sort == i)),i] = 1
-        for i in range(m-k,m):
-            W[:, i] = vind (V[:,i])
-        T = np.dot(V[:,:m].T, W[:,:m])
+        m += lasit_newvec #m is size of current subspace Hamiltonian
+       
+        if i == 0:              #first step
+            sort = hdiag.argsort()
+            for i in range(0,m):
+                V[int(np.argwhere(sort == i)),i] = 1   #positions with lowest values set as 1
+        for i in range(0,m):
+            W[:, i] = vind (V[:,i])   #Hv, create transformed guess vectors
+        T = np.dot(V[:,:m].T, W[:,:m])  # T is subspace Hmailtonian
         THETA,S = np.linalg.eigh(T)  #Diagonalize the subspace Hamiltonian.
         idx = THETA.argsort()
         theta = THETA[idx]    #eigenvalues
         s = S[:,idx]          #eigenkets, m*m
-        sum_norm = 0
-        for j in range(0,k):
-            residual = np.dot((W[:,:m]- theta[j]*V[:,:m]), s[:,j])
+        
+        sum_convec = 0
+        lasit_newvec = 0
+        for i in range(0,k):      #looking at first k vecrors one by one, check if they are roots
+            residual = np.dot((W[:,:m]- theta[i]*V[:,:m]), s[:,i])
             norm = np.linalg.norm(residual)
-            d = hdiag-theta[j]
-            d[d<1e-8 ] = 1e-8
-            new_vec = residual/d
-            V[:,(m+j)] = new_vec
-            if norm < tol:
-                sum_norm = sum_norm +1
-        if sum_norm == k:
-            #print ('All', sum_norm, 'Guess Vectors Converged')
+                   # number of added new guess vectors from last iteration.
+            if norm > tol:         # norm > tol means we didn't find correct eigenkets, so we create new guess vectors
+                d = hdiag-theta[i]
+                d[(d<1e-8)&(d>=0)] = 1e-8
+                d[(d>-1e-8)&(d<0)] = -1e-8   #kick out all small values
+                new_vec = residual/d          #new guess vectors, core step of Davidson method
+                for i in range (0, m + lasit_newvec):
+                    new_vec = new_vec - np.dot(V[:,i], new_vec) * V[:,i]   #/ np.linalg.norm(v1)) = 1 should be after np.dot
+                norm = np.linalg.norm (new_vec)
+                if norm > 1e-15:
+                    new_vec = new_vec/norm
+                    V[:, m + lasit_newvec] = new_vec
+                    lasit_newvec += 1
+            else:
+                sum_convec += 1
+        print ('sum_convec =', sum_convec)
+        print ('lasit_newvec =', lasit_newvec)
+        if sum_convec == k:
             break
-        #Gram-Schimidt block,
-        for p in range(0, k):
-            for q in range (0, m+p):
-                V[:,m+p] = orthonormal(V[:,q], V[:,m+p])
-    #print (sum_norm)
-    print ('Iteration =', Iteration)
+                    
+                   
+    #print ('Iteration =', Iteration)
+    
     end = time.time()
-    Eigenkets = np.dot(V[:,:m], s[:, :eig])
+    Eigenkets = np.dot(V[:,:m], s[:, :k])
     print ('Davidson time:', round(end-start,4))
-    return (theta[:eig])
+    return (theta[:k])
 
 print (27.21138624598853 * davidson(vind,3))
 
