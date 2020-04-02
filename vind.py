67bf36b5a559e5b3ee9def410cad41e26d8b47ec
John Zhou <zzz6@case.edu>
2020-04-02 14:45:27 -0400

    small revise

--- vind.py
+++ vind.py
@@ -3,12 +3,16 @@ import numpy as np
 from pyscf import gto, scf, dft, tddft
 import davidson4
 mol = gto.Mole()
-mol.build(atom = 'C         -5.14247        3.17333        0.00000;\
-O         -4.07247        3.17333        0.00000;\
-H         -5.49914        2.49946       -0.75072;\
-H         -5.49914        4.16041       -0.20823;\
-H         -5.49914        2.86013        0.95895;\
-H         -3.74914        3.45727       -0.86933', basis = '631g', symmetry = True)
+mol.build(atom = 'Na 0 0 0; F 0 0 1', basis = '631g', symmetry = True)
+
+
+
+#mol.build(atom = 'C         -5.14247        3.17333        0.00000;\
+#O         -4.07247        3.17333        0.00000;\
+#H         -5.49914        2.49946       -0.75072;\
+#H         -5.49914        4.16041       -0.20823;\
+#H         -5.49914        2.86013        0.95895;\
+#H         -3.74914        3.45727       -0.86933', basis = '631g', symmetry = True)
 mf = dft.RKS(mol) #RKS, restrict the geometry, no optimization #mf is ground density?
 mf.xc = 'b3lyp'
 mf.kernel()  #single point energy
@@ -32,15 +36,13 @@ def davidson(vind, k): # fucntion vind and how many eignvalues to solve
     V = np.zeros((n,30*k)) #array of zeros. a container to hold guess vectors
     W = np.zeros((n,30*k)) #array of zeros. a container to hold transformed guess vectors
   
+    
     # Begin iterations
     Iteration   = 0
-    
-    m = k  # m is size of initial subspace Hamiltonian, amount of initial guesses   #m=k works for H2, m=4k works for H2O
-    lasit_newvec = 0  #it records amount of new vector added in last iteration, ranging from 1 to k
+    m = 4*k  # m is size of initial subspace Hamiltonian, amount of initial guesses   #m=k works for H2, m=4k works for H2O
     for i in range(0, max):
         Iteration = Iteration + 1
-        m += lasit_newvec #m is size of current subspace Hamiltonian
-
+        
         if i == 0:              #first step
             sort = hdiag.argsort()
             for j in range(0,m):
@@ -54,11 +56,11 @@ def davidson(vind, k): # fucntion vind and how many eignvalues to solve
         s = S[:,idx]          #eigenkets, m*m
         
         sum_convec = 0
-        lasit_newvec = 0
+        lasit_newvec = 0  #it records amount of new vector added in last iteration, ranging from 1 to k
         for x in range(0,k):      #looking at first k vecrors one by one, check if they are roots
             residual = np.dot((W[:,:m]- theta[x]*V[:,:m]), s[:,x])
             norm = np.linalg.norm(residual)
-                   # number of added new guess vectors from last iteration.
+                 
             if norm > tol:         # norm > tol means we didn't find correct eigenkets, so we create new guess vectors
                 d = hdiag-theta[i]
                 d[(d<1e-8)&(d>=0)] = 1e-8
@@ -78,6 +80,8 @@ def davidson(vind, k): # fucntion vind and how many eignvalues to solve
                 sum_convec += 1
 #        print ('sum_convec =', sum_convec)
 #        print ('lasit_newvec =', lasit_newvec)
+        
+        m += lasit_newvec   #now m is size of subspace Hamiltonian in next iteration
         if sum_convec == k:
             break
                     
