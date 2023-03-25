c50760f612e5a461a308244423f60a8ceb71ad84
John <zzz6@case.edu>
2023-03-25 22:41:44 +0800

    jacob

--- TDA/TDA.py
+++ TDA/TDA.py
@@ -36,17 +36,18 @@ TDA           [7.2875264  8.93645089 9.18027002 9.92054961 10.16937337]
 '''
 def main():
 
+    TDA_iter_preconditioner2 = TDA_iter_preconditioner
     if args.jacobi:
         # TDA_ip_dict[0] = (TDA_iter_initial_guess, Jacobi_preconditioner)
-        TDA_iter_preconditioner = Jacobi_preconditioner
+        TDA_iter_preconditioner2 = Jacobi_preconditioner
     if args.projector:
         # TDA_ip_dict[0] = (TDA_iter_initial_guess, projector_preconditioner)
-        TDA_iter_preconditioner = projector_preconditioner
+        TDA_iter_preconditioner2 = projector_preconditioner
 
     TDA_ip_dict = gen_ip_func(diag_i = TDA_diag_initial_guess,
                               iter_i = TDA_iter_initial_guess,
                               diag_p = TDA_diag_preconditioner,
-                              iter_p = TDA_iter_preconditioner)
+                              iter_p = TDA_iter_preconditioner2)
 
 
     for option in args.ip_options:
